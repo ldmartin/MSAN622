a6e675a848f46112cf570e416a1cff377acbd3eb
ldmartin <layla.d.martin@gmail.com>
2015-04-17 20:37:43 -0700

    updating hw2 files

--- hw2/homework2script2.js
+++ hw2/homework2script2.js
@@ -30,7 +30,7 @@ var color = d3.scale.ordinal().range(["#e7ba52", "#de9ed6", "#17becf"]);
 
 //append title to chart:
 var title = d3.select(".chart-title").text('State education and wealth by US region');
-var description = d3.select(".chart-desctiption").text("Brush a region to highlight corresponding points in all plots.");
+//var description = d3.select(".chart-desctiption").text("Brush a region to highlight corresponding points in all plots.");
 
 
 d3.csv("data/stateData5.csv", function(error, data) {
